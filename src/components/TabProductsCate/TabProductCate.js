638c20fc7b6b16ec71233fd80dc848859eb1a223
huynhluan491 <huynhluan491@gmail.com>
2023-02-26 12:21:05 +0700

    ~

--- src/components/TabProductsCate/TabProductCate.js
+++ src/components/TabProductsCate/TabProductCate.js
@@ -18,7 +18,7 @@ const tabTitle = [
 ];
 
 function TabProductCate() {
-    const [value, setValue] = useState('1');
+    const [value, setValue] = useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
