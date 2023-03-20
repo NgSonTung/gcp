b0894b7676c2bd0b436e5e9fa52e8dfb1ec736b0
huynhluan491 <huynhluan491@gmail.com>
2023-03-20 08:28:10 +0700

    !

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,22 +5,9 @@ import TextTitle from './HomeTextTitle/HomeTextTitle';
 import HomeProductBestSale from './HomeProductBestSale/index';
 import SliderBanner from './SliderBanner/index';
 import TabProductCate from '~/components/TabProductsCate/TabProductCate';
-import { useState, useEffect } from 'react';
 const cx = classNames.bind(styles);
 
 function Home() {
-    const [products, setProducts] = useState();
-    useEffect(() => {
-        fectchingProducts();
-        console.log(products);
-    }, []);
-
-    const fectchingProducts = async () => {
-        const fectchedData = await fetch('http://localhost:3001/');
-        const result = await fectchedData.json();
-        setProducts(result.data.products);
-        console.log(result);
-    };
     return (
         <div className={cx('wrapper')}>
             <SliderBanner />
@@ -29,9 +16,6 @@ function Home() {
             <TabProductCate />
             <HomeProductBestSale cate={'phone'} srcImgBanner={require('~/assets/images/anh-sale-tet.png')} />
             <HomeProductBestSale cate={'laptop'} srcImgBanner={require('~/assets/images/phu-kien-hot.png')} />
-            {products?.map((item, index) => (
-                <p>{item.name}</p>
-            ))}
         </div>
     );
 }
