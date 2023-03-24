9e4e52079db56111e5d000a9525c98040937cd3b
hai <minhhaitrandang@gmail.com>
2023-03-24 17:40:49 +0700

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,7 @@
+const express = require("express");
 const userController = require("../controllers/user");
 const router = express.Router();
-const express = require("express");
 
 router.route("/").get(userController.getAllUser);
+
+module.exports = router;
