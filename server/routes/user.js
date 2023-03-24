d1699371185fd08170609b946e32689d1128e154
huynhluan491 <huynhluan491@gmail.com>
2023-03-24 17:44:08 +0700

    deo biet

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
+const express = require("express");
 const userController = require("../controllers/user");
 const router = express.Router();
-const express = require("express");
 
 router.route("/").get(userController.getAllUser);
