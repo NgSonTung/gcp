2fb4aa9e3dad164090cd495bc64527dfbccb6d3d
hai <minhhaitrandang@gmail.com>
2023-04-04 00:45:10 +0700

    401 :)))

---
+++ client/src/components/401/Page401.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './Page401.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(style);
+
+const Page401 = () => {
+    return (
+        <div>
+            <div className={cx('wrapper')}>
+                <img src={require('./401-1.jpg')} alt="401" />
+                <div className={cx('wrapper-text')}>
+                    <div>
+                        <p>Get F****** out</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Page401;
--- client/src/pages/Products/Keyboard/Keyboard.js
+++ client/src/pages/Products/Keyboard/Keyboard.js
@@ -1,5 +1,10 @@
+import Page401 from '~/components/401/index';
 function Keyboard() {
-    return <div>Keyboard</div>;
+    return (
+        <div>
+            <Page401 />
+        </div>
+    );
 }
 
 export default Keyboard;
