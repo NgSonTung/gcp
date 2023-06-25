931bfb7b28bce6f50d56a75da341c80ceb28a8d9
ngsontung <ngsondecon@gmail.com>
2023-06-25 11:15:22 +0700

    abc

--- server/routes/brand.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const brandController = require("../controllers/Brand");
-const router = express.Router();
-
-router.route("/").get(brandController.getAllBrands);
-router.route("/:id").get(brandController.getBrandById);
-
-module.exports = router;
